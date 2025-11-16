969bcffd1c8b9be1135e43acf5ab94ecbe30eeaf
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2025-11-16 12:21:19 -0500

    Update hdf5 libs to v2.0.0

--- libs/lib_linux_gcc11_x86_64/include/H5RTmodule.h
+++ libs/lib_linux_gcc11_x86_64/include/H5RTmodule.h
@@ -4,7 +4,7 @@
  *                                                                           *
  * This file is part of HDF5.  The full HDF5 copyright notice, including     *
  * terms governing use, modification, and redistribution, is contained in    *
- * the COPYING file, which can be found at the root of the source code       *
+ * the LICENSE file, which can be found at the root of the source code       *
  * distribution tree, or in https://www.hdfgroup.org/licenses.               *
  * If you do not have access to either file, you may request a copy from     *
  * help@hdfgroup.org.                                                        *
@@ -12,16 +12,17 @@
 
 /*
  * Purpose: This file contains declarations which define macros for the
- *          H5FD driver package.  Including this header means that the source file
- *          is part of the H5FD driver package.
+ *          H5RT package.  Including this header means that the source file
+ *          is part of the H5RT package.
  */
-#ifndef H5FDdrvr_module_H
-#define H5FDdrvr_module_H
+#ifndef H5RTmodule_H
+#define H5RTmodule_H
 
 /* Define the proper control macros for the generic FUNC_ENTER/LEAVE and error
  *      reporting macros.
  */
-#define H5_MY_PKG     H5FD
-#define H5_MY_PKG_ERR H5E_FILE
+#define H5RT_MODULE
+#define H5_MY_PKG_ERR  H5E_RTREE
+#define H5_MY_PKG_INIT NO
 
-#endif /* H5FDdrvr_module_H */
+#endif /* H5RTmodule_H */
--- libs/lib_macos_x86_64/include/H5TSmodule.h
+++ libs/lib_macos_x86_64/include/H5TSmodule.h
@@ -4,7 +4,7 @@
  *                                                                           *
  * This file is part of HDF5.  The full HDF5 copyright notice, including     *
  * terms governing use, modification, and redistribution, is contained in    *
- * the COPYING file, which can be found at the root of the source code       *
+ * the LICENSE file, which can be found at the root of the source code       *
  * distribution tree, or in https://www.hdfgroup.org/licenses.               *
  * If you do not have access to either file, you may request a copy from     *
  * help@hdfgroup.org.                                                        *
@@ -12,16 +12,18 @@
 
 /*
  * Purpose: This file contains declarations which define macros for the
- *          H5FD driver package.  Including this header means that the source file
- *          is part of the H5FD driver package.
+ *          H5TS package.  Including this header means that the source file
+ *          is part of the H5TS package.
  */
-#ifndef H5FDdrvr_module_H
-#define H5FDdrvr_module_H
+#ifndef H5TSmodule_H
+#define H5TSmodule_H
 
 /* Define the proper control macros for the generic FUNC_ENTER/LEAVE and error
  *      reporting macros.
  */
-#define H5_MY_PKG     H5FD
-#define H5_MY_PKG_ERR H5E_FILE
+#define H5TS_MODULE
+#define H5_MY_PKG      H5TS
+#define H5_MY_PKG_ERR  H5E_THREADSAFE
+#define H5_MY_PKG_INIT NO
 
-#endif /* H5FDdrvr_module_H */
+#endif /* H5TSmodule_H */
